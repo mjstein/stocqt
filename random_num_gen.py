8e41a6000dfa6de2aee3b682ebf4a7b913306d1f
Michael Stein <msuddd@gmail.com>
2015-07-26 01:41:30 +0100

    add series gen

--- random_num_gen.py
+++ random_num_gen.py
@@ -1,6 +1,13 @@
+from random import randint
+
 class Random_num_gen():
     def __init__(self, min, max):
         self.min = min
         self.max = max
-    def generate_random_number(self):
-        pass
+    def _generate_random_number(self):
+      return randint(self.min,self.max)        
+    def return_random_number_series(self, series_length):
+        random_series=[]
+        for i in range(0,series_length):
+           random_series.append(self._generate_random_number)
+        return random_series
--- unit_tests/random_num_gen_test.py
+++ unit_tests/random_num_gen_test.py
@@ -6,12 +6,16 @@ class Test_Random_num_gen(unittest.TestCase):
     def setUp(self):
       self.min = 1
       self.max = 2
+      self.series_length = 2
       self.rg = Random_num_gen(self.min,self.max)
         
 
     def test_generate_random_number(self):
-        rand_num = self.rg.generate_random_number()
+        rand_num = self.rg._generate_random_number()
         self.assertTrue((self.min <= rand_num) and (self.max >= rand_num))
+    def test_return_random_number_series(self):
+       rand_series = self.rg.return_random_number_series( self.series_length) 
+       self.assertTrue(len(rand_series) == self.series_length)
          
 if __name__ == '__main__':
  unittest.main()
