5ebc08669dd9cdc5b81024322056bb40a0635e7f
Michael Stein <msuddd@gmail.com>
2015-07-26 01:14:28 +0100

    further additions to unit tests

--- random_num_gen.py
+++ random_num_gen.py
@@ -2,5 +2,5 @@ class Random_num_gen():
     def __init__(self, min, max):
         self.min = min
         self.max = max
-    def generate_random_number():
+    def generate_random_number(self):
         pass
--- unit_tests/random_num_gen_test.py
+++ unit_tests/random_num_gen_test.py
@@ -1,7 +1,17 @@
 import unittest
+from random_num_gen import Random_num_gen
 
 class Test_Random_num_gen(unittest.TestCase):
-  def test_upper(self):
-            self.assertEqual('foo'.upper(), 'FOO')
+
+    def setUp(self):
+      self.min = 1
+      self.max = 2
+      self.rg = Random_num_gen(self.min,self.max)
+        
+
+    def test_generate_random_number(self):
+        rand_num = self.rg.generate_random_number()
+        self.assertTrue((self.min <= rand_num) and (self.max >= rand_num))
+         
 if __name__ == '__main__':
  unittest.main()
